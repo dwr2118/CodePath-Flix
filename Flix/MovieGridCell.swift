b66d72b6fc1254caf8a05ae7d5a236811becd50f
dwr2118 <dwr2118@columbia.edu>
2022-09-23 23:15:23 -0400

    made part2 of tflix

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Diego Rivas Lazala on 9/23/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
